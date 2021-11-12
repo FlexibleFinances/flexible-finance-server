2dc92012b15ce3db4d7108803efb5b0b020ed540
Mike Warburton <mike.j.warburton@protonmail.com>
2021-11-12 04:13:40 -0500

    ready sequelize for heroku & local

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 const Sequelize = require('sequelize');
-sequelize = new Sequelize(process.env.DATABASE_URL, {
+
+sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DEV_DATABASE_URL, {
     dialectOptions: {
       ssl: {
-        require: true,
+        require: process.env.DATABASE_URL ? true : false,
         rejectUnauthorized: false
       }
     }
@@ -28,3 +21,10 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
