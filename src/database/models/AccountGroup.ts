be857214d7834b9d6cb0afc3700d2ffae5fe9430
Mike Warburton <>
2021-12-10 01:03:23 -0500

    describe basic API elements for primary classes

--- src/database/models/AccountGroup.ts
+++ src/database/models/AccountGroup.ts
@@ -1,4 +1,15 @@
-import { DataTypes, Model } from "sequelize";
+import {
+  Association,
+  DataTypes,
+  HasManyAddAssociationMixin,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  HasManyGetAssociationsMixin,
+  HasManyHasAssociationMixin,
+  HasManySetAssociationsMixin,
+  Model,
+} from "sequelize";
+import { Account } from ".";
 import sequelize from "../index";
 
 interface AccountGroupAttributes {
@@ -13,6 +24,22 @@ export class AccountGroup extends Model implements AccountGroupAttributes {
   public readonly updatedAt!: Date;
 
   public name!: string;
+
+  public readonly accounts?: Account[];
+
+  public static override associations: {
+    accounts: Association<Account, AccountGroup>;
+  };
+
+  // Since TS cannot determine model association at compile time
+  // we have to declare them here purely virtually
+  // these will not exist until `Model.init` was called.
+  public getAccount!: HasManyGetAssociationsMixin<Account>;
+  public setAccount!: HasManySetAssociationsMixin<Account, number>;
+  public addAccounts!: HasManyAddAssociationMixin<Account, number>;
+  public hasAccounts!: HasManyHasAssociationMixin<Account, number>;
+  public countAccount!: HasManyCountAssociationsMixin;
+  public createAccounts!: HasManyCreateAssociationMixin<Account>;
 }
 
 AccountGroup.init(
