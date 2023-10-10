3acd8c0257b000140f793f96bcc2b5e9d10e8e7c
Mike Warburton <>
2023-10-10 01:39:57 -0400

    FFS-4; proof of concept with FieldType

--- src/app/controllers/fieldType.controller.ts
+++ src/app/controllers/fieldType.controller.ts
@@ -1,8 +1,14 @@
-import { CreationAttributes, FindOptions, Op, WhereOptions } from "sequelize";
+import {
+  type CreationAttributes,
+  type FindOptions,
+  Op,
+  type WhereOptions,
+} from "sequelize";
 import FieldType from "../../database/models/FieldType";
+import { FieldTypeResponse } from "../models/FieldTypeResponse";
 import { defaultLimit } from "../../utils/constants";
-import express from "express";
-import { fieldTypeTypeEnum } from "../../utils/enumerators";
+import type express from "express";
+import { type fieldTypeTypeEnum } from "../../utils/enumerators";
 import { hasRequestArguments } from "../../utils/helperFunctions";
 
 export async function getFieldType(
@@ -44,7 +50,10 @@ export async function createFieldType(
     validator: req.body.validator,
   };
   const fieldType = await FieldType.create(createOptions);
-  res.status(200).send({ message: "FieldType created.", fieldType });
+  const fieldTypeResponse = new FieldTypeResponse(fieldType);
+  res
+    .status(200)
+    .send({ message: "FieldType created.", fieldType: fieldTypeResponse });
 }
 
 export async function updateFieldType(
---
+++ src/app/models/FieldTypeResponse.ts
@@ -0,0 +1,24 @@
+import type FieldType from "../../database/models/FieldType";
+
+export class FieldTypeResponse {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+
+  name: string;
+  type?: string;
+  validator?: string;
+  fieldTypeComponentIds?: number[];
+  fieldIds?: number[];
+
+  constructor(fieldType: FieldType) {
+    this.id = fieldType.id;
+    this.createdAt = fieldType.createdAt.toISOString();
+    this.updatedAt = fieldType.updatedAt.toISOString();
+    this.name = fieldType.name;
+    this.type = fieldType.type;
+    this.validator = fieldType.validator;
+    this.fieldTypeComponentIds = fieldType.FieldTypeComponentIds;
+    this.fieldIds = fieldType.FieldIds;
+  }
+}
